f6981655a7c3e7099df0a33c161e98acdcf32e50
Muzammil Momin <mominmuzammil157@gmail.com>
2025-07-15 20:47:11 +0530

    created server using express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('my first mern app');
+const express = require('express');
+// const connectDB = require('./config/db');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+});
