2c5e4f29d8bb064aeaa994f6606fda546c3fd5f5
Muzammil Momin <mominmuzammil157@gmail.com>
2025-07-15 23:50:37 +0530

    Added mongoose package and created user schema and model using it. Created signup API and added a few users to the database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
 const express = require('express');
-// const connectDB = require('./config/db');
+const {connectDB} = require('./config/database');
+const { UserModel } = require('./models/user');
 
 const app = express();
-
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`server listening on port ${port}`)
+app.post('/signup', async (req, res) => {
+    const userObj = {
+        firstName: 'sachin',
+        lastName: 'tendli',
+        emailId: 'sachin@gmail.com',
+        age: 36,
+        password: 'sachin1234'
+    }
+try {
+    const user = new UserModel(userObj);
+
+    await user.save();
+
+    res.status(200).json({message:'new user created'})
+} catch (error) {
+    res.status(400).json({message: error.message})
+}
+    
+})
+
+
+connectDB()
+  .then(() => {
+    console.log('database connected');
+    app.listen(port, () => {
+        console.log(`server listening on port ${port}`)
+    });
+})
+  .catch(err => {
+    console.log(err);
 });
+
--- src/config/database.js
+++ src/config/database.js
@@ -1,32 +1,9 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 
-const uri = 'mongodb+srv://NodeMomin:9941nBu7xQwiFjL0@codeflaircluster.eluyhyf.mongodb.net/';
+const uri = 'mongodb+srv://NodeMomin:9941nBu7xQwiFjL0@codeflaircluster.eluyhyf.mongodb.net/codeFlair';
 
-const client = new MongoClient(uri);
-
-const dbName = 'helloWorld';
-
-async function main() {
-  await client.connect();
-  console.log('Connected successfully to database');
-  const db = client.db(dbName);
-  const collection = db.collection('users');
-
-  const data = {
-    first: 'dilip',
-    last: 'trophy'
-  }
-
-  await collection.insertOne(data);
-
-  const results = await collection.find({}).toArray();
-
-  console.log('found documents =>', results);
-
-  return 'operation done.';
+const connectDB = async () => {
+  await mongoose.connect(uri);
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+module.exports = {connectDB}
