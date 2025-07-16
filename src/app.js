e2c31944e1e7315ed7346d8c32f7c0c1b64f239c
Muzammil Momin <mominmuzammil157@gmail.com>
2025-07-16 18:27:37 +0530

    created CRUD APIs

--- src/app.js
+++ src/app.js
@@ -3,34 +3,65 @@ const {connectDB} = require('./config/database');
 const { UserModel } = require('./models/user');
 
 const app = express();
-const port = 3000;
+app.use(express.json())
 
-app.post('/signup', async (req, res) => {
-    const userObj = {
-        firstName: 'sachin',
-        lastName: 'tendli',
-        emailId: 'sachin@gmail.com',
-        age: 36,
-        password: 'sachin1234'
+app.post('/signup', async (req, res) => {    
+    try {
+        const user = new UserModel(req.body);
+        await user.save();
+
+        console.log('user saved into db');
+        res.status(200).json({message:'new user created'})
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({Error: 'User already exist'})
     }
-try {
-    const user = new UserModel(userObj);
+});
 
-    await user.save();
+app.get("/feed", async (req, res) => {
+    try {
+        // const user = new UserModel(req.body);
+        const allUsers = await UserModel.find({});        
+        res.status(200).json({data: allUsers})
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({Error: error.message})
+    }
+});
 
-    res.status(200).json({message:'new user created'})
-} catch (error) {
-    res.status(400).json({message: error.message})
-}
-    
+app.post("/profile", async (req, res) => {
+    try {
+        const user = await UserModel.findOne({_id: req.body.userId})
+        res.status(200).json({data: user})
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({Error: 'user not found'})
+    }
+});
+
+app.delete('/profile', async (req, res) => {
+    try {
+        const user = await UserModel.findOneAndDelete({_id: req.body.userId});
+        res.status(200).json({message: `${user.firstName} deleted successfully`})
+    } catch (error) {
+        res.status(400).json({Error: 'user not found'})   
+    }
+});
+
+app.patch('/profile', async (req, res) => {
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(req.body.userId, req.body, {returnDocument:"after"});
+        res.status(200).json({message:'user updated succesfully', data: updatedUser});
+    } catch (error) {
+        res.status(400).json({Error: error.message});
+    }
 })
-
 
 connectDB()
   .then(() => {
     console.log('database connected');
-    app.listen(port, () => {
-        console.log(`server listening on port ${port}`)
+    app.listen(3000, () => {
+        console.log(`server listening on port 3000`)
     });
 })
   .catch(err => {
