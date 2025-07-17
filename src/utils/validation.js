d5293fa40d862a870b2686bfe2f8bacb5d584df5
Muzammil Momin <mominmuzammil157@gmail.com>
2025-07-18 01:57:48 +0530

    modified validate signup data function to sanitize firstname and lastname fields

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,11 +4,11 @@ const validateSignupData = (req) => {
 
     const {firstName, lastName, emailId, password} = req.body;
 
-    if (!firstName || !lastName ) {
-        throw new Error("Enter a name between 3-10 characters");
-    }
-    else if (!validator.isEmail(emailId)) {
+    if (!validator.isEmail(emailId)) {
         throw new Error("Enter a valid email address");
+    }
+    else if (!firstName || !lastName ) {
+        throw new Error("Enter name between 3-10 characters");
     }
     else if (!validator.isStrongPassword(password)) {
         throw new Error("Enter a strong password");
