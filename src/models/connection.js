3e022cbfbe62606ecc9ed6b617c11053c46ee270
Muzammil Momin <mominmuzammil157@gmail.com>
2025-07-22 13:00:37 +0530

    created PRE schema method to check sender and reciever are same

--- src/models/connection.js
+++ src/models/connection.js
@@ -19,6 +19,16 @@ const connectionRequestSchema = new mongoose.Schema({
     }
 },{timestamps: true});
 
+connectionRequestSchema.pre('save', function(next) {
+    
+    const connectionRequest = this;
+
+    if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+        throw new Error("Cannot send request to yourself");
+    }
+    next()
+})
+
 const ConnectionRequestModel = new mongoose.model('ConnectionRequest', connectionRequestSchema);
 
 module.exports = {ConnectionRequestModel}
